5644b93f4c7f395a2a409d4a3643e4304e667e9d
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-07-05 19:59:33 -0400

    Initial commit and completed exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            double celsiusTemp = TempConverter.FahrenheitToCelsius(64);
+            double tempInFahrenheit = 64;
+            celsiusTemp = Math.Round(celsiusTemp, 2);
+            Console.WriteLine($"The temperature is {tempInFahrenheit}\u00B0F or {celsiusTemp}\u00B0C");
+
+            double fahrenheitTemp = TempConverter.CelsiusToFahrenheit(24);
+            double tempInCelsius = 24;
+            fahrenheitTemp = Math.Round(fahrenheitTemp, 2);
+            Console.WriteLine($"The temperature is {tempInCelsius}\u00B0C or {fahrenheitTemp}\u00B0F");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempInFahrenheit)
+        {
+            double celsius = (tempInFahrenheit - 32) * 5 / 9;
+            return celsius;
+
+            
+
+        }
+
+        public static double CelsiusToFahrenheit(double tempInCelsius)
+        {
+            double fahrenheit = (tempInCelsius * 9 / 5) + 32;
+            return fahrenheit;
+
+            
+
+
+        }
+
+
+
+    }
+}
